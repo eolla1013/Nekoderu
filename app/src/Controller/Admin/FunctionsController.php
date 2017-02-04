9c7915722850c98beffb8a888594cfed95aae286
Shinobu Izumi <stagesp1@gmail.com>
2017-02-04 18:32:25 +0000

    max execution time unlimited

--- app/src/Controller/Admin/FunctionsController.php
+++ app/src/Controller/Admin/FunctionsController.php
@@ -35,11 +35,14 @@ class FunctionsController extends AppController
             $num = $post['num'];
              
             $memoryLimit = ini_get('memory_limit');
+            $maxExecutionTime = ini_get('max_execution_time');
             ini_set('memory_limit', '512M');
+            ini_set("max_execution_time", 0);
 
             $this->GoogleApi->inputTNRDataFromGoogleDrive($sheetId,$folderId,$num);
             
             ini_set('memory_limit', $memoryLimit);
+            ini_set("max_execution_time", $maxExecutionTime);
             
             exit;
              
